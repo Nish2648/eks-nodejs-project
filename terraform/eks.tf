2de2f626faf182e728caaa5bcec66ba2a9b1df31
Nish2648 <networknisha@gmail.com>
2026-06-21 22:10:05 +0530

    VPC and EKS mosule created

---
+++ terraform/eks.tf
@@ -0,0 +1,28 @@
+module "eks" {
+
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+
+    default = {
+
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
